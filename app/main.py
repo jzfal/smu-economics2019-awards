10fb5b99f593896e15a5dcc15544f2c35406d0a1
Naufal <juznauf@gmail.com>
2020-06-06 16:52:47 +0800

    Refactor main module
    Defined standard function to extract
    and save transcript to csv

--- app/main.py
+++ app/main.py
@@ -5,45 +5,38 @@ from datetime import timedelta
 import pandas as pd
 
 
+def extract_to_csv(URL = 'https://www.youtube.com/watch?v=4HgRQT6qDto', start = (0, 16,35) , end = (1,9,0) , filename = 'all_subs1.csv'):
 
-yt = YouTube('https://www.youtube.com/watch?v=4HgRQT6qDto')
-en_captions = yt.captions.get_by_language_code('en')
-subs = list(srt.parse(en_captions.generate_srt_captions())) # generate list of tuples
+    """
+    default url is commencement for econs 
+    """
+    yt = YouTube(URL)
+    en_captions = yt.captions.get_by_language_code('en')
+    subs = list(srt.parse(en_captions.generate_srt_captions())) # generate list of tuples
 
+    # find start and end index
+    start_index = 0
+    for i in range(len(subs)):
+        if subs[i].start  < timedelta(hours = start[0],minutes = start[1], seconds = start[2]):
+            continue
+        else:
+            start_index = i
+            break
+    end_index = 0
+    for i in range(start_index,len(subs)):
+        if subs[i].start > timedelta(hours = end[0], minutes = end[1],
+        seconds= end[2]):
+            end_index = i
+            break
+    content_dict = {'content':[]}
+    for i in range(start_index, end_index +1):
+        content_dict['content'].append(subs[i].content)
 
-
-
-# find start and end index
-start_index = 0
-for i in range(len(subs)):
-    if subs[i].start  < timedelta(minutes = 16, seconds = 35):
-        continue
-    else:
-        start_index = i
-        break
-end_index = 0
-for i in range(start_index,len(subs)):
-    if subs[i].start > timedelta(hours = 1, minutes = 9):
-        end_index = i
-        break
-# print(subs[start_index])
-# print(subs[end_index])
-
-content_dict = {'content':[]}
-for i in range(start_index, end_index +1):
-    content_dict['content'].append(subs[i].content)
-
-
-df1 = pd.DataFrame(data = content_dict)
-df1.to_csv('all_subs.csv', index = False)
-# write func
-def write_to_csv():
-    with open('all_subs.csv','w') as outfile:
-        spamwriter = csv.writer(outfile, delimiter=' ',quotechar=' ', quoting=csv.QUOTE_MINIMAL)
-        for i in range(start_index, end_index):
-            spamwriter.writerow(subs[i].content)
+    df1 = pd.DataFrame(data = content_dict)
+    df1.to_csv(filename, index = False)
 
 
-
-
-
+if __name__ == "__main__":
+    # to find data for law commencement 
+    extract_to_csv(URL = 'https://www.youtube.com/watch?v=jTk1yuiUnvE',
+    start = (0, 46, 25), end=(1,15,48), filename= 'law_commencement_2019.csv')
